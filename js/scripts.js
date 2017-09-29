f748bc29fff578f8b5d2bb6bb54b5b5648efb550
sravanthi velaga <svelaga0412@gmail.com>
2017-09-29 16:00:56 -0700

    JS logic for ordering pizza

---
+++ js/scripts.js
@@ -0,0 +1,62 @@
+
+//Business Logic
+function Pizza(cheese, size, pizza, topping) {
+  this.cheese = cheese;
+  this.size = size;
+  this.pizza = pizza;
+  this.basePrice = 10;
+  this.topping = [];
+}
+
+
+Pizza.prototype.fullPrice = function(){
+  // if (this.size === 'large'){
+  //   this.basePrice += 2;
+  // } else if (this.size === 'extraLarge'){
+  //   this.basePrice += 6;
+  // } else {
+  //   this.basePrice = 10;
+  // }
+
+  if (this.size === 'medium') {
+      this.basePrice += this.topping.length;
+  } else if (this.size === 'large') {
+      this.basePrice += this.topping.length;
+  } else if (this.size === 'extraLarge') {
+      this.basePrice += this.topping.length;
+  }
+
+  if(this.cheese === 'extra'){
+    this.basePrice += 1;
+  }
+  return this.pizza*this.basePrice;
+  console.log(this.basePrice)
+};
+
+
+
+//FrontEnd Logic
+
+$(function(){
+  $("form.formOne").submit(function(event){
+    event.preventDefault();
+
+    var selectedCheese = $("select#cheese").val();
+    var selectedSize = $("select#size").val();
+    var inputtedNumber = parseInt($("input#pizza").val());
+
+    $("input:checkbox[name=topping]:checked").each(function() {
+     var topping = $(this).val();
+     newPizza.topping.push(topping);
+   });
+
+    var newPizza = new Pizza(selectedCheese, selectedSize, inputtedNumber, topping);
+    var cost = newPizza.fullPrice();
+
+    $(".size").text(newPizza.size);
+    $(".pizza").text(inputtedNumber);
+    $("#cost").text(cost*inputtedNumber);
+    $("#result").show();
+  })
+
+});
